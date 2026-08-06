29355f48d5418258778e1b03377f08b0febec0e7
Swan Htet Zaw <htetzawswan@gmail.com>
2026-08-06 10:16:58 +0800

    Initialise CyberPulse EDU project

---
+++ app/src/main/java/au/edu/jcu/cyberpulseedu/MainActivity.kt
@@ -0,0 +1,47 @@
+package au.edu.jcu.cyberpulseedu
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import au.edu.jcu.cyberpulseedu.ui.theme.CyberPulseEDUTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CyberPulseEDUTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CyberPulseEDUTheme {
+        Greeting("Android")
+    }
+}
